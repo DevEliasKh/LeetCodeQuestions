136b5c53f99b73713f62a2dee1fca79703f9087a
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-05-10 13:55:43 +0330

    add rotate solution

---
+++ Easy/Array/rotate.js
@@ -0,0 +1,19 @@
+var rotate = function (nums, k) {
+   let temp = nums.length > k ? k : k % nums.length;
+   let del = nums.splice(0, nums.length - temp);
+
+   for (const num of del) {
+      nums.push(num);
+   }
+
+   console.log(nums);
+
+   //******************************
+   // this code take too long
+   //  for (let i = 0; i < k; i++){
+   //      nums.unshift(nums.pop())
+   //  }
+};
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+rotate(arr, 4);

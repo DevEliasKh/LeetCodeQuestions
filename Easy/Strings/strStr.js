343ad6636879366934de5f3c8ffebcf97cf4fc66
develiaskh <develiaskh@gmail.com>
2024-06-29 16:42:21 +0330

    add strStr

---
+++ Easy/Strings/strStr.js
@@ -0,0 +1,23 @@
+//leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+https: var strStr = function (haystack, needle) {
+   let pOne,
+      pTwo = 0;
+   let result = -1;
+   while (pTwo < haystack.length) {
+      if (haystack.pTwo == needle.pOne) {
+         ++pOne;
+         ++pTwo;
+         // recurive fn
+      } else {
+         pOne = 0;
+      }
+   }
+};
+
+function fn(pOne, pTwo) {}
